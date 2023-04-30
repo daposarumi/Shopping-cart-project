b0e6f7bf9617285d17aa1b835f9b184592ed9fc3
daposarumi <sarumiadedapo@gmail.com>
2023-04-30 21:35:54 +0100

    add sign

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const appSettings = {
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppinglistInDB = ref(database, "shoppingList")
-const shoppingListEl = document.getElementById(shopping-list)
+const shoppingListEl = document.getElementById("shopping-list")
 
 
 const inputFieldEl = document.getElementById("input-field")
