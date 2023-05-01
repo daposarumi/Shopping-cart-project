1d6ec76ad41e3f826dc547f0dc60522d33dd80dc
daposarumi <sarumiadedapo@gmail.com>
2023-05-01 17:29:02 +0100

    new code

--- functions.js
+++
@@ -1,8 +0,0 @@
-export function clearInputValue() {
-    inputFieldEl.value = ""
-
-}
-
-export function appendtoShoppingList(itemValue) {
-    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
-}
