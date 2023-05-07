533629a0756f7e9858c6955bc2af223a62a97c52
daposarumi <sarumiadedapo@gmail.com>
2023-05-07 02:17:32 +0100

    superb!

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 
 const appSettings = {
@@ -24,11 +24,22 @@ addButtonEl.addEventListener("click", function() {
     push(shoppinglistInDB, inputValue)
 
     clearInputFieldEl()
-
-
-    appendItemToshoppingListEl(inputValue)
     
 })
+
+onValue(shoppinglistInDB, function(snapshot) {
+    let shoppingListArray = Object.values(snapshot.val())
+
+    clearShoppingListEl()
+
+    for (let i = 0; i < shoppingListArray.length; i++) {
+        appendItemToshoppingListEl(shoppingListArray[i])
+    }
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
 
 function clearInputFieldEl() {
     inputFieldEl.value = ""
